7f2656a1136d66952e3c2fbdb6ad8051c0154f67
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-04-06 22:29:25 -0300

    Aplicaçao finalizada

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,22 @@
+interface RepositoryItemProps {
+    repository: {
+        name: String;
+        description: String;
+        html_url: string
+    }
+}
+
+export function RepositoryItem(props: RepositoryItemProps){
+    return (
+        <li>
+            <strong>{props.repository.name}</strong>
+            <p>{props.repository.description}</p>
+
+            
+
+            <a href={props.repository.html_url}>
+                Acessar repositorio
+            </a>
+        </li>
+    )
+}
